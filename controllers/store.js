e66d4060441110d14324e82fd11be3c8c36da95c
Diego Alexander Ramirez Castaneda <dalexander.ramirez@globant.com>
2024-06-22 10:26:55 -0500

    ya se hizo el script de productos

---
+++ controllers/store.js
@@ -0,0 +1,37 @@
+let name = document.getElementById('name');
+let price = document.getElementById ('price');
+let description = document.getElementById ('description');
+let photo = document.getElementById('photo');
+let depotView = document.getElementById ('depot');
+let buttonEnviar = document.getElementById ('buttonEnviar');
+
+let products = [];
+
+window.addEventListener('load', function(){
+    let saveProducts = this.localStorage.getItem('saveProducts');
+    if(saveProducts) {
+        usuarios = JSON.parse(saveProducts);
+    }
+});
+
+buttonEnviar.addEventListener('click', function(event) {
+    alert('Su compra fue exitosa')
+    event.preventDefault()
+    let product = {
+        name:name.value,
+        price:price.value,
+        description:description.value,
+        photo:photo.value,
+        depotView:depotView.value
+
+    }
+    products.push(product);
+    localStorageSend();
+})
+
+
+function localStorageSend(){
+localStorage.setItem('products',JSON.stringify(products));
+        alert("¡Su compra fue exitosa, gracias por preferirnos!");
+
+}
