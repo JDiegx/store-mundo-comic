42478573d964aa0ea8a63d67d3ac4f284829e606
Juanglb28 <juanglb28@gmail.com>
2024-06-22 10:18:03 -0500

    User register

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,26 @@
-let hola;
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let registerButton = document.getElementById('registerButton');
+
+let usuarios = [];
+
+registerButton.EventListener('click', function(){
+    Event.preventDefault();
+    let usuario = {
+        name: username.value,
+        password: userpassword.value,
+        register: registerButton,
+    }
+
+    usuarios.push(usuario);
+    localStorage.send();
+})
+
+
+function localStoragesend(){
+    localStorage.setItem('usuarios',JSON.stringify('usuarios'));
+    console.log('Success');
+    console.log(localStorage.getItem(usuarios));
+
+}
+
